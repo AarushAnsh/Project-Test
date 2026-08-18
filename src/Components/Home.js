a02271ec1b0152ba48412ea5bb1d904e2fc35028
tarun <tarunlal07@gmail.com>
2026-08-18 20:54:41 +0530

    feat/filter movie with searchkey

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,22 +1,14 @@
-import React, { useContext } from "react";
-import Searchbar from "./Search bar/Searchbar";
+import React, { useEffect, useState } from "react";
 import Movie from "./Movielist/Movie";
 import useFetch from "../hooks/useFetch";
 import Spinner from "./Spinner";
 import usePagination from "../hooks/usePagination";
 import PaginationButton from "./PaginationButton/PaginationButton";
-// import { MovieContext } from "../Context/MoveData";
+import axios from "axios";
+
 
 const Home = () => {
-  // const {
-  //   movie,
-  //   setMovie,
-  //   loading,
-  //   error,
-  //   searchQuery,
-  //   setSearchQuery,
-  //   submitHandler,
-  // } = useContext(MovieContext);
+  const [searchKey, setSearchKey] = useState("");
 
   const {
     data: movie,
@@ -24,13 +16,47 @@ const Home = () => {
     loading,
   } = useFetch("https://api.tvmaze.com/shows");
 
-  const { startIndex, endIndex, currentPage, totalPage, prevPage, nextPage } =
-    usePagination(movie, 10);
+  //map,filter,reduce -> array , => new array
 
-  const paginatedMovie = movie?.slice(startIndex, endIndex);
+  
+
+
+  const [filterMovie, setFilterMovie] = useState([]);
+
+  useEffect(() => {
+    if (movie.length > 0) setFilterMovie(movie);
+  }, [movie]);
+
+  useEffect(() => {
+    const fetchSearchedData = async () => {
+      try {
+        const getMovieData = await axios(
+          `https://api.tvmaze.com/search/shows?q=${searchKey}`
+        );
+
+        const transformMovieData = getMovieData.data.map((movie) => {
+          return movie.show;
+        });
+        setFilterMovie(transformMovieData);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    if (searchKey) fetchSearchedData();
+    else setFilterMovie(movie);
+  }, [searchKey]);
+
+  const { startIndex, endIndex, currentPage, totalPage, prevPage, nextPage } =
+    usePagination(filterMovie, 5);
+
+  const onSearchHandler = (e) => {
+    setSearchKey(e.target.value);
+  };
+
+  const paginatedMovie = filterMovie?.slice(startIndex, endIndex);
 
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       {/* <Searchbar
         searchQuery={searchQuery}
         setSearchQuery={setSearchQuery}
@@ -38,6 +64,12 @@ const Home = () => {
       /> */}
       {loading && <Spinner />}
       {error && <p className="text-sm text-red-600">Something went wrong</p>}
+      <input
+        onChange={onSearchHandler}
+        value={searchKey}
+        className="border border-gray-300 outline-none w-60 rounded-md p-1.5"
+        placeholder="Search Movie..."
+      />
       {movie.length > 0 ? (
         <Movie movie={paginatedMovie} />
       ) : (
